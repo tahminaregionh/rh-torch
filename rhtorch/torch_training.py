7934667fe7d96a64986432c1cb6285ed50cddab5
RaphaelSura <raphaeldaveau@protonmail.com>
2021-05-19 10:24:12 +0200

    cleaned loading of config file, updated transfer learning part

--- rhtorch/torch_training.py
+++ rhtorch/torch_training.py
@@ -8,6 +8,7 @@ from torch.utils.data import DataLoader
 import os
 import sys
 from pathlib import Path
+
 # library package imports
 from rhtorch.models import modules
 from rhtorch.callbacks import plotting
@@ -21,44 +22,39 @@ def main():
     parser.add_argument("-i", "--input", 
                         help="Project directory. Should contain data folder to train on + config file + data_generator. Will use current working directory if nothing passed", 
                         type=str, default=os.getcwd())
-    parser.add_argument("-c", "--config", help="Config file else than {project_name}_config.yaml in input folder", type=str, default='')
+    parser.add_argument("-c", "--config", help="Config file else than 'config.yaml' in project directory (input dir)", type=str, default='config.yaml')
     parser.add_argument("-k", "--kfold", help="K-value for selecting train/test split subset. Default k=0", type=int, default=0)
     parser.add_argument("-t", "--test", help="Test run for 1 patient", action="store_true", default=False)
-    parser.add_argument("--precision", help="Torch precision. Default 32", type=int, default=32)
+    # parser.add_argument("-p", "--precision", help="Torch precision. Default 32", type=int, default=32)
+    
     args = parser.parse_args()
-
     project_dir = Path(args.input)
     is_test = args.test
 
-    # load configs and create additional entries for saving later
-    configs, model_outname = load_model_config(project_dir, Path(args.config))
-    if is_test:
-        print('This is a test run on 10/2 train/test patients and 5 epochs.')
-        configs['epoch'] = 5
-        os.environ['WANDB_MODE'] = 'dryrun'
+    # load configs from file + additional info from args
+    configs = load_model_config(project_dir, args)
 
-    model_outname += f"_k{args.kfold}_e{configs['epoch']}"
-    configs['project_dir'] = str(project_dir)
-    configs['model_name'] = model_outname
-    configs['k_fold'] = args.kfold
-    configs['precision'] = args.precision
-    configs['config_file'] = args.config
-    
     # Set local data_generator
     sys.path.insert(1, args.input)
     import data_generator
     loader_params = {'batch_size': configs['batch_size'], 'num_workers': 4}
     data_gen = getattr(data_generator, configs['data_generator'])
     
+    # setting up test
+    if is_test:
+        print('This is a test run on 10/2 train/test patients and 5 epochs.')
+        configs['epoch'] = 5
+        os.environ['WANDB_MODE'] = 'dryrun'
+        
     # training data
     augment = False if 'augment' not in configs else configs['augment']
     if augment:
         print("Augmenting data")
-    data_train = data_gen(project_dir, 'train', conf=configs, augment=augment, test=is_test)
+    data_train = data_gen('train', conf=configs, augment=augment, test=is_test)
     train_dataloader = DataLoader(data_train, shuffle=True, **loader_params)
     
     # validation data
-    data_valid = data_gen(project_dir, 'valid', conf=configs, augment=False, test=is_test)
+    data_valid = data_gen('valid', conf=configs, augment=False, test=is_test)
     valid_dataloader = DataLoader(data_valid, **loader_params)
     
     # define lightning module
@@ -68,9 +64,25 @@ def main():
     
     # transfer learning setup
     if 'pretrained_generator' in configs:
-        assert configs['pretrained_generator'].endswith('.pt'), "When transfer learning, you must do it from a .pt file.\nTL from checkpoints is not yet implemented."
-        model.load_state_dict(torch.load(configs['pretrained_generator']), strict=False)
+        print("Setting up transfer learning")
+        pretrained_model_path = Path(configs['pretrained_generator'])
+        if pretrained_model_path.exists():
+            if pretrained_model_path.name.endswith(".ckpt"):
+                # important to pass in new configs here as we want to load the weights but config may differ from pretrained model
+                model = module.load_from_checkpoint(pretrained_model_path, hparams=configs, in_shape=shape_in, strict=False)
+            elif pretrained_model_path.endswith(".pt"):
+                # this works for both .pt and .ckpt actually
+                # WARNING I don't know which of the above or below method is the correct way to load ckpt
+                # this below method only load the weights. Above also load state of optimizer, etc...
+                ckpt = torch.load(pretrained_model_path)
+                pretrained_model = ckpt['state_dict']
+                model.load_state_dict(pretrained_model, strict=False)
+            else:
+                raise ValueError("Expected model format: '.pt' or '.ckpt'.")
+        else:
+            raise FileNotFoundError("Model file not found. Check your path in config file.")
         
+    
     # wandb dashboard setup
     wandb_logger = WandbLogger(name=configs['version_name'],
                                project=configs['project_name'],
@@ -78,16 +90,16 @@ def main():
                                save_dir=project_dir,
                                config=configs)
     
-    # CALLBACKS
+    # callbacks
     callbacks = []
     if 'callback_image2image' in configs:
-        data_plot = data_gen(project_dir, 'valid', conf=configs, augment=False, test=is_test)
+        data_plot = data_gen('valid', conf=configs, augment=False, test=is_test)
         plot_dataloader = DataLoader(data_plot, **loader_params)
         callback_image2image = getattr(plotting, configs['callback_image2image'])
-        callbacks.append( callback_image2image(plot_dataloader) )
+        callbacks.append(callback_image2image(plot_dataloader))
     
     # checkpointing
-    model_path = project_dir.joinpath('trained_models').joinpath(model_outname)
+    model_path = project_dir.joinpath('trained_models').joinpath(configs['model_name'])
     model_path.mkdir(parents=True, exist_ok=True)
     checkpoint_dir = model_path.joinpath('checkpoints')
     checkpoint_dir.mkdir(parents=True, exist_ok=True)
@@ -103,7 +115,7 @@ def main():
         save_last=True,     # additionally overwrites a file last.ckpt after each epoch
         period=2,
     )
-    callbacks.append( checkpoint_callback )
+    callbacks.append(checkpoint_callback)
     
     # set the trainer and fit
     trainer = pl.Trainer(max_epochs=configs['epoch'], 
@@ -113,6 +125,7 @@ def main():
                          accelerator='ddp',
                          resume_from_checkpoint=existing_checkpoint,
                          auto_select_gpus=True,
+                         accumulate_grad_batches=configs['acc_grad_batches'],
                          precision=configs['precision'],
                          profiler="simple")
 
@@ -123,7 +136,7 @@ def main():
     configs['best_model'] = checkpoint_callback.best_model_path
 
     # save the model
-    output_file = model_path.joinpath(f"{model_outname}.pt")
+    output_file = model_path.joinpath(f"{configs['model_name']}.pt")
     torch.save(model.state_dict(), output_file)
     copy_model_config(model_path, configs)
     print("Saved model and config file to disk")
