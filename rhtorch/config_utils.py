04c6ecf690828db2c65a69aff7be394cb90f2a59
Claes Ladefoged <claesnl@gmail.com>
2021-05-23 09:04:59 +0200

    Modified model name output to not use global

--- rhtorch/config_utils.py
+++ rhtorch/config_utils.py
@@ -65,7 +65,7 @@ class UserConfig:
         
         data_shape = 'x'.join(map(str, self.hparams['data_shape']))
         base_name = f"{self.hparams['module']}_{self.hparams['version_name']}_{self.hparams['data_generator']}"
-        dat_name = f"bz{self.hparams['global_batch_size']}_{data_shape}"
+        dat_name = f"bz{self.hparams['batch_size']}_{data_shape}"
         self.hparams['model_name'] = f"{base_name}_{dat_name}_k{self.args.kfold}_e{self.hparams['epoch']}"
     
     def save_copy(self, output_dir, append_timestamp=False):
