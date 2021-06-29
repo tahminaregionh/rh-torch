ae63332700b5255fffa9eaf503b939eea2963ea1
Claes Ladefoged <claesnl@gmail.com>
2021-06-29 11:04:42 +0200

    Fixed bug with when running test

--- rhtorch/torch_training.py
+++ rhtorch/torch_training.py
@@ -67,9 +67,9 @@ def main():
 
     # Set local data_generator
     sys.path.insert(1, args.input)
-    import data_generator 
+    import data_generator
     data_gen = getattr(data_generator, configs['data_generator'])
-    data_module = data_gen(configs)
+    data_module = data_gen(configs, quick_test=is_test)
     data_module.prepare_data()
     data_module.setup()
     print('Done preparing the data.')
